0a2efea0f7db7968c1c9148432b08254b61d31bc
LAPTOP-ORREJKSR\zhanghongfeng <zhang415436123@163.com>
2020-09-09 10:24:23 +0800

    pytest

---
+++ Suit/test_account_balance.py
@@ -0,0 +1,33 @@
+
+from Service.wbf_signature import Signature
+import time
+from Service.config import environment
+import pytest
+
+class TestOpenApi():
+
+    def setup(self):
+        self.api_key = environment(0)[0]
+        self.secret_key = environment(0)[1]
+        self.host = environment(0)[-2]
+        self.tie = int(time.time())
+        print('测试用例开始执行!')
+
+    def teardown(self):
+        print('测试用例执行结束!')
+
+    def test_balance(self):
+        request_path = '/open/api/user/account'
+        result = Signature(self.api_key, self.secret_key, self.tie).get_sign(request_path, self.host)
+        print('查询资产成功response:{}'.format(result))
+        assert result['msg'] == 'suc' and result['code'] == '0'
+
+    def test_fail(self):
+        request_path = '/open/api/user/account'
+        result = Signature(self.api_key, self.secret_key[:1], self.tie).get_sign(request_path, self.host)
+        print('查询资产失败response:{}'.format(result))
+        assert result['code'] == '100004'
+
+
+if __name__ == '__main__':
+    pytest.main("-s  test_abc.py")
